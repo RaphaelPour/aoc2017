826a5650c718faacaeeaae3570ca525c5b41b5a4
Raphael Pour <info@raphaelpour.de>
2022-10-21 12:47:13 +0200

    days 07: add bones

---
+++ days/07/part.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/RaphaelPour/stellar/input"
+	
+)
+
+func part1(towers []string) string {
+	
+}
+
+func main() {
+	towers := input.LoadDefaultString()
+	fmt.Println(part1(towers))
+}
